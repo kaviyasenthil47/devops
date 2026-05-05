c90153ca1571000f2c8421ffef70681422cd1d99
kaviyasenthil47 <kaviyasenthil47@gmail.com>
2026-05-05 12:54:00 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,6 @@
+a=20
+b=10
+print("addition of  ",a," and  ",b," is ",a+b)
+print("subraction of  ",a," and  ",b," is ",a-b)
+print("multiplication of  ",a," and  ",b," is ",a*b)
+print("division of  ",a," and  ",b," is ",a/b)
